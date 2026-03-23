2c033ba48aab32313dac1eb4c647be24304db9f1
dinesh <dineshsuwalkya31@gmail.com>
2026-03-23 21:08:48 +0530

    Sum

---
+++ 6_Recursion/2_sum.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int Sum(int n) {
+    // base case
+    if(n == 1) {
+        return 1;
+    }
+
+    return n + Sum(n-1);
+}
+
+int main() {
+    cout<<Sum(5)<<endl;
+
+    return 0;
+}
